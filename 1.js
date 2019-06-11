5f2f7a1e9fa66c72da72e30007c3129814f8ff4d
huy <htl.ice14@gmail.com>
2019-06-11 18:39:30 +0700

    add project

---
+++ 1.js
@@ -0,0 +1,21 @@
+﻿ var app = angular.module('myApp',['ngRoute','ui.select', 'ngSanitize']);
+
+
+
+ app.config(function ($routeProvider, $locationProvider) {
+ 	$locationProvider.html5Mode(true);
+
+ 	$routeProvider.when('/', {
+ 		templateUrl: 'content1.html',
+ 	})
+ 	$routeProvider.when('/about', {
+ 		templateUrl: 'about.html',
+ 	})
+ 	$routeProvider.when('/post', {
+ 		templateUrl: 'post.html',
+ 	}) 	
+ 	$routeProvider.when('/contact', {
+ 		templateUrl: 'contact.html',
+ 	})
+ 	.otherwise({ redirectTo: '/' })
+ })
